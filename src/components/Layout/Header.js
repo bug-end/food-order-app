c8772a699b8ad2f9c29b2556b74133fbe03b33fb
lukasz.bielecki <lukasz.bielecki@hycom.pl>
2023-02-11 16:55:20 +0100

    food order app update

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,10 @@
-import { Fragment } from "react";
-
 import classes from "./Header.module.css";
-
 import mealsImage from "../../assets/meals.jpg";
 import HeaderCartButton from "./HeaderCartButton";
 
-const Header = (props) => {
+const Header = props => {
   return (
-    <Fragment>
+    <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
         <HeaderCartButton onClick={props.onShowCart} />
@@ -15,7 +12,7 @@ const Header = (props) => {
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A tabel full of delicious food!" />
       </div>
-    </Fragment>
+    </>
   );
 };
 
